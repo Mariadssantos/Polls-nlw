a6da9a27589907b565d8cb9f11c39a7f297da662
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2024-02-08 08:54:03 -0300

    create connection with websocket in server

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import websocket  from '@fastify/websocket'
+import { pollResult } from './ws/poll-results'
 
 const app = fastify()
 
@@ -11,10 +13,12 @@ secret: "polls-app-nlw", // for cookies signature
 hook: 'onRequest', 
 })
 
+app.register(websocket)
 app.register(createPoll),
 app.register(getPoll),
 app.register(voteOnPoll)
 
+app.register(pollResult)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server running on port 3333')
