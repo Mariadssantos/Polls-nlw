5bebdfdc2d5b3e6bc6eaec7e04a1785e4a97f474
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2024-02-08 08:58:30 -0300

    zincrby redis in votes

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { redis } from '../../lib/redis';
+import { voting } from '../../utils/votes-pub-sub';
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -24,15 +26,22 @@ export async function voteOnPoll(app: FastifyInstance) {
                     sessionId_pollId: { sessionId, pollId }
                 }
             })
-            if(userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId != pollOptionId) {
+            if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId != pollOptionId) {
                 await prisma.vote.delete({
                     where: {
                         id: userPreviousVoteOnPoll.id
                     }
                 })
+
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
+                })
             } else if (userPreviousVoteOnPoll) {
-                return reply.status(400).send({message: 'You already voted on this poll'})
+                return reply.status(400).send({ message: 'You already voted on this poll' })
             }
+
         }
 
 
@@ -53,6 +62,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 pollId,
                 pollOptionId,
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
         })
 
         return reply.status(201).send();
